8dbcb3fcf8f4ea13b790c66bc7aa390de8ff9a40
Sabri Durmaz <drmz.sbr@gmail.com>
2025-07-31 15:36:10 +0300

    08.2 Methods - More Exercise

---
+++ 08.2 Methods - More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class DataTypes
+{
+    public static void Main(string[] args)
+    {
+        string command = Console.ReadLine();
+        string input = Console.ReadLine();
+
+        switch (command)
+        {
+            case "int":
+                if (int.TryParse(input, out int intValue))
+                {
+                    MultiplyBy2(intValue);
+                }
+                break;
+            case "real":
+                if (double.TryParse(input, out double doubleValue))
+                {
+                    RealMultiplyAndFormat(doubleValue);
+                }
+                break;
+            case "string":
+                StringFormattedPrint(input);
+                break;
+        }
+    }
+
+    private static void MultiplyBy2(int inp)
+    {
+        inp *= 2;
+        Console.WriteLine(inp);
+    }
+
+    private static void RealMultiplyAndFormat(double num)
+    {
+        num *= 1.5;
+        Console.WriteLine(num.ToString("F2"));
+    }
+
+    private static void StringFormattedPrint(string input)
+    {
+        Console.WriteLine($"${input}$");
+    }
+}
